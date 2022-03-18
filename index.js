7c331dc640fdada706d38e092d712479d6dabfe1
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-03-18 15:28:08 -0500

    commit tree 045cb5900cd877770da52f14a5d58b74df51669b

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// document.addEventListener("DomContentLoaded", function () {
+//    let hey = documentgetElementById('text').textContent = "This is really cool!";;
+//     return hey;
+// }
+// )
+let text = document.getElementById('text');
+  document.addEventListener("DOMContentLoaded", (event) => {
+  text.textContent = "This is really cool!"
+  })
